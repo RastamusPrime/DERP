b92e74e5c17024e270b6e806f44990bd7c192833
nick <nrod07@gmail.com>
2015-10-22 15:06:42 -0700

    yeee muffins

--- app.js
+++ app.js
@@ -5,17 +5,25 @@ angular.module('myDerp', [])
        onDerpComplete = function(response)
       {
             s.user = response.data;
+            h.get(s.user.repos_url)
+                  .then(onRepos, onError);
+      };
+
+      onRepos=function(response)
+      {
+                  s.repos=response.data;
       };
 
       onError = function(reason){
-            s.error = "You're an Idoit!!!<br />Put in a real person.";
+            s.error = "You're an Idoit!!! Put in a real person.";
       };
 
       s.search = function(username){
       h.get("https://api.github.com/users/" + username)
       .then(onDerpComplete, onError);
-
+            };
       s.username="angular";
       s.message= "GO ROOOOOOOYYAAALS!";
-};
+      s.repoSortOrder= "-stargazers_counts";
+
 }]);
